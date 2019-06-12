f628cd12e09fc1c607bbb86ae427947bb13dbd3a
Jess Wang <wangjess@umich.edu>
2019-06-12 10:12:48 -0400

    Created a User and Micropost model and controller. Done with toy app!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_214348) do
+ActiveRecord::Schema.define(version: 2019_06_12_134158) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
